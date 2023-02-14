d9632a6016fb20ee41dfca06154f5d1afb6d31db
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-14 13:01:24 +0700

    Update try.py

--- try.py
+++ try.py
@@ -1,11 +1,6 @@
 
 import streamlit as st
-import subprocess
-
-url = "https://github.com/ranaroussi/yfinance.git"
-target_dir = "/path/to/destination/directory"
-
-subprocess.run(["git", "clone", url, target_dir], check=True)
+from git import Repo
 st.title("Stock Price App")
 st.write("hello")
 st.write("testing")
