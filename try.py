4e9de8c6f5bd56d8ea29c83eb847856584a71b92
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-14 13:11:33 +0700

    Update try.py

--- try.py
+++ try.py
@@ -4,6 +4,9 @@ import subprocess
 repo_url = "https://github.com/ranaroussi/yfinance.git"
 local_dir = "ranaroussi/yfinance"
 subprocess.run(["git", "clone", repo_url, local_dir])
+import sys
+sys.path.insert(0, local_dir + "/yfinance") # replace `local_dir` with the actual path to your cloned yfinance directory
+
 st.title("Stock Price App")
 st.write("hello")
 st.write("testing")
