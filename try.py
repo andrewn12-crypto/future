1eb1b0807983747e0e4a04033148811227a6bb0a
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-14 13:05:13 +0700

    Update try.py

--- try.py
+++ try.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 from git import Repo
-Repo.clone_from("https://github.com/ranaroussi/yfinance.git")
+gh repo clone ranaroussi/yfinance
 st.title("Stock Price App")
 st.write("hello")
 st.write("testing")
