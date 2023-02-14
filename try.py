fbf965591457c905e7f136a1a6133dc365169015
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-14 13:16:15 +0700

    Update try.py

--- try.py
+++ try.py
@@ -3,7 +3,7 @@ import streamlit as st
 import subprocess
 repo_url = "https://github.com/ranaroussi/yfinance.git"
 local_dir = "/path/to/local/directory"
-subprocess.run(["git", "clone", repo_url, local_dir])
+subprocess.run(["git", "clone", repo_url, future])
 import sys
 sys.path.insert(0, local_dir + "/yfinance") # replace `local_dir` with the actual path to your cloned yfinance directory
 import yfinance
