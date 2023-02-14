4b4b252c5440d8cec2cbbf9a51f28e47e71b1c1f
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-14 13:00:35 +0700

    Update try.py

--- try.py
+++ try.py
@@ -1,6 +1,11 @@
 
 import streamlit as st
-git clone https://github.com/ranaroussi/yfinance.git
+import subprocess
+
+url = "https://github.com/ranaroussi/yfinance.git"
+target_dir = "/path/to/destination/directory"
+
+subprocess.run(["git", "clone", url, target_dir], check=True)
 st.title("Stock Price App")
 st.write("hello")
 st.write("testing")
