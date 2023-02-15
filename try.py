fa20c0919ec849e6ba01fbd2b2b76de42b992f41
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-15 16:03:57 +0700

    Update try.py

--- try.py
+++ try.py
@@ -24,3 +24,14 @@ data3=data3['Close']
 data_to_predict1=data1[:(np.shape(data1)[0]-1)]
 data_to_predict2=data2[:(np.shape(data2)[0]-1)]
 data_to_predict3=data3[:(np.shape(data3)[0]-1)]
+model1 = auto_arima(data_to_predict1, seasonal=False, error_action='ignore', suppress_warnings=True, stepwise=True, trace=False)
+model2 = auto_arima(data_to_predict2, seasonal=False, error_action='ignore', suppress_warnings=True, stepwise=True, trace=False)
+model3 = auto_arima(data_to_predict3, seasonal=False, error_action='ignore', suppress_warnings=True, stepwise=True, trace=False)
+
+
+forecast1 = model1.predict(n_periods=1)
+forecast2 = model2.predict(n_periods=1)
+forecast3 = model3.predict(n_periods=1)
+print("1H",forecast1)
+print("1,5H",forecast2)
+print("1D",forecast3)
