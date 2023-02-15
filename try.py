ebbd778c0a63fae3d6dd9de52eb9be1beb844f84
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-15 16:25:31 +0700

    Update try.py

--- try.py
+++ try.py
@@ -5,13 +5,5 @@ import pandas as pd
 import pmdarima
 from pmdarima import auto_arima
 
-# Download data asli 2010-2023 EURUSD
 
-data2 = yf.download('EURUSD=X', interval="90m",period="60D")
-data2.reset_index(inplace=True)
-data2=data2['Close']
-data_to_predict2=data2[:(np.shape(data2)[0]-1)]
-model2 = auto_arima(data_to_predict2, seasonal=False, error_action='ignore', suppress_warnings=True, stepwise=True, trace=False)
-forecast2 = model2.predict(n_periods=1)
-st.write("1,5H",forecast2)
 
