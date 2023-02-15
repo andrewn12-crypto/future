3560efc39d81daf8114293ae06288c1eb7aa46d9
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-15 16:32:43 +0700

    Update try.py

--- try.py
+++ try.py
@@ -15,3 +15,10 @@ model2 = auto_arima(data_to_predict2, seasonal=False, error_action='ignore', sup
 forecast2 = model2.predict(n_periods=1)
 st.write('1,5H',forecast2)
 
+
+
+data3=data3['Low']
+data_to_predict3=data3[:(np.shape(data3)[0]-1)]
+model3 = auto_arima(data_to_predict3, seasonal=False, error_action='ignore', suppress_warnings=True, stepwise=True, trace=False)
+forecast3 = model3.predict(n_periods=1)
+st.write('1,5H',forecast3)
