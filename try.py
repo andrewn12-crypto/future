549f3ec0787db425ccc5602de8cd30d402b4c221
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-15 15:45:20 +0700

    Update try.py

--- try.py
+++ try.py
@@ -1,28 +1,3 @@
 import streamlit as st
-import subprocess
-
-# Specify the repository URL and the absolute path to the local directory where you want to clone it
-repo_url = "https://github.com/ranaroussi/yfinance.git"
-local_dir = "/path/to/local/directory"
-
-# Clone the repository using Git
-subprocess.run(["git", "clone", repo_url, local_dir])
-import subprocess
-import sys
-
-# Clone yfinance repository
-repo_url = "https://github.com/ranaroussi/yfinance.git"
-local_dir = "/path/to/local/directory"
-subprocess.run(["git", "clone", repo_url, local_dir])
-subprocess.run(["pip", "install", repo_url, local_dir])
-import yfinance
-# Add cloned directory to sys.path
-sys.path.insert(0, local_dir)
-
-# Import yfinance module
-
-
-# Use the yfinance module in your Streamlit app
-st.title("Stock Price App")
-st.write("hello")
-st.write("testing")
+python -m pip install
+pip install git+https://github.com/jkbr/httpie.git
