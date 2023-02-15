d1306b3dcac9daa6ffcc472f6a8f4bbe7a2cad34
andrewn12-crypto <125338731+andrewn12-crypto@users.noreply.github.com>
2023-02-15 16:00:40 +0700

    Update try.py

--- try.py
+++ try.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import yfinance as yf
-
+import numpy as np
+import pandas as pd
 
 # Download data asli 2010-2023 EURUSD
 data1 = yf.download('EURUSD=X', interval="1m",period="7D")
